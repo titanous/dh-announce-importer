2772e75e18d03d68226b9b1f444079da7fe3e7ff
Jonathan Rudenberg <jon335@gmail.com>
2009-04-20 20:19:18 -0400

    step one form and styles

--- importer.rb
+++ importer.rb
@@ -1,12 +1,26 @@
 require 'rubygems'
 require 'sinatra'
+
+# File importers
 require 'roo'
+require 'fastercsv'
+
+# Dreamhost API wrapper
 require 'dreamy'
+
+# options
+enable :sessions
+
 
 get '/' do
   haml :index
 end
 
+post '/upload' do
+  @file = params[:file][:tempfile]
+end
+
+# css
 get '/css/application.css' do
   content_type 'text/css', :charset => 'utf-8'
   sass :application
